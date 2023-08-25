11600cdb8dd294fabcecc8d2be45ba74991429e6
qutluq <kootlook.applications@gmail.com>
2023-08-25 18:17:26 +0600

    feat: add text-editor

---
+++ src/components/quill-editor/formats.ts
@@ -0,0 +1,23 @@
+export const formats = [
+    // 'background',
+    "bold",
+    "color",
+    "font",
+    "code",
+    "italic",
+    "link",
+    "size",
+    "strike",
+    "script",
+    "underline",
+    "blockquote",
+    "header",
+    "indent",
+    "list",
+    "align",
+    "direction",
+    "code-block",
+    "formula",
+    // 'image' // disallowed
+    // 'video' // disallowed
+  ];
---
+++ src/components/quill-editor/types.ts
@@ -0,0 +1,10 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import type ReactQuill from "react-quill";
+import type { ReactQuillProps } from "react-quill";
+
+export interface RQ extends ReactQuillProps {
+  forwardedRef?: React.Ref<ReactQuill>;
+  value?: string;
+  onChange?: Dispatch<SetStateAction<string>>;
+}
